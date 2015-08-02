e99495313def65b2aa4d5fe7cc749bafb4aea246
Jean <holderbaumjean@gmail.com>
2015-08-02 19:19:19 -0300

    Fun with Forms (8)

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+
+def add_page(request, category_name_slug):
+
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+			
+				return category(request, category_name_slug)
+		else:
+			print form.errors
+	else:
+		form = PageForm()
+
+	context_dict = {'form':form, 'category': cat}
+
+	return render(request, 'rango/add_page.html', context_dict)
+
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+
+			return index(request)
+		else:
+			print form.errors
+	else:
+		form = CategoryForm()
+
+	return render(request, 'rango/add_category.html', {'form': form})
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -16,26 +57,15 @@ def about(request):
 	return render(request, 'rango/about.html', context_dict)
 
 def category(request, category_name_slug):
-
 	context_dict = {}
-
 	try:
 		category = Category.objects.get(slug=category_name_slug)
 		context_dict['category_name'] = category.name
-
-        # Retrieve all of the associated pages.
-        # Note that filter returns >= 1 model instance.
 		pages = Page.objects.filter(category=category).order_by('-views')[:5]
 
-        # Adds our results list to the template context under name pages.
 		context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
 		context_dict['category'] = category
 	except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything - the template displays the "no category" message for us.
 		pass
 
-    # Go render the response and return it to the client.
 	return render(request, 'rango/category.html', context_dict)
