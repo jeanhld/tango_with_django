be9b973e4427638e9cfb834bba60d9cd442c86b4
Jean <holderbaumjean@gmail.com>
2015-08-02 15:51:20 -0300

    Exercises from Chapter 5 and Chapter 6 with exercises

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,13 @@ def index(request):
 	context_dict = {
 		'boldmessage': "I'm bold font from the context"
 	}
-	
+
 	return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+	context_dict = {
+		'about_message': "See a picture of me:"
+	}
+
+	return render(request, 'rango/about.html', context_dict)
 
